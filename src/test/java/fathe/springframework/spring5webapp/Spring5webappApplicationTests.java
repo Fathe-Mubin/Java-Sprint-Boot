0e52097352d0f8bcf7494d632560c61497ae98e4
fathe.mubin <fathe.mubin@bjitgroup.com>
2020-05-14 16:31:01 +0600

    first commit

---
+++ src/test/java/fathe/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package fathe.springframework.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
